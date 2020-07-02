a3d1f7b40f5afa124654b37e139f3f99b4ca7fd6
kudakwashe <kuda@nightsbridge.co.za>
2020-07-02 21:09:02 +0200

    Initial git commit

---
+++ src/main/java/com/cb/farmerapp/FarmerAppApplication.java
@@ -0,0 +1,13 @@
+package com.cb.farmerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmerAppApplication.class, args);
+	}
+
+}
