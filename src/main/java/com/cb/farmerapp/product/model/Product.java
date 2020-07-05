423ae223ceb8039315917250d0a9058cd376e809
Garwe <67763320+Garwe@users.noreply.github.com>
2020-07-05 12:45:11 +0200

    Added Product Getter and Setters and Constructors

---
+++ src/main/java/com/cb/farmerapp/product/model/Product.java
@@ -0,0 +1,69 @@
+package com.cb.farmerapp.product.model;
+
+public class Product {
+    private int Id;
+    private String productName;
+    private  String msisdn;
+    private String idNo;
+    private String customer;
+    private String season;
+    private String color;
+
+    public int getId() {
+        return Id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public String getMsisdn() {
+        return msisdn;
+    }
+
+    public String getIdNo() {
+        return idNo;
+    }
+
+    public String getCustomer() {
+        return customer;
+    }
+
+    public String getSeason() {
+        return season;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public void setFarmerName(String productName) {
+        this.productName = productName;
+    }
+
+    public void setMsisdn(String msisdn) {
+        this.msisdn = msisdn;
+    }
+
+    public void setIdNo(String idNo) {
+        this.idNo = idNo;
+    }
+
+    public void setCustomer(String customer) {
+        this.customer = customer;
+    }
+
+    public void setSpeciality(String speciality) {
+        this.season = speciality;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
+
+
