1b790b928beab7d466783f12fe205deb8c958792
Garwe <67763320+Garwe@users.noreply.github.com>
2020-07-06 19:20:54 +0200

    removed getters and setters and added lombok and annotation

--- src/main/java/com/cb/farmerapp/product/model/Product.java
+++ src/main/java/com/cb/farmerapp/product/model/Product.java
@@ -2,6 +2,7 @@ package com.cb.farmerapp.product.model;
 import lombok.Getter;
 import lombok.Setter;
 @Getter @Setter
+@Data
 public class Product {
     private int Id;
     private char productName;
