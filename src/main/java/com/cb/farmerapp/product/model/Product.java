9173099ff45422bf39a37dece415fa0ecdcae133
Garwe <67763320+Garwe@users.noreply.github.com>
2020-07-07 19:11:30 +0200

    Added Product Getter and Setters and Constructors

--- src/main/java/com/cb/farmerapp/product/model/Product.java
+++ src/main/java/com/cb/farmerapp/product/model/Product.java
@@ -1,19 +1,36 @@
 package com.cb.farmerapp.product.model;
-import lombok.Data;
+
 import lombok.Getter;
 import lombok.Setter;
-@Getter @Setter
-@Data
-public class Product {
-Long Id;
-  private String productName;
-    private  String msisdn;
-    private String idNo;
-    private String customer;
-    private String season;
-    private String color;
+import lombok.ToString;
 
- 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "product")
+@ToString
+@Access(AccessType.FIELD)
+@Getter
+@Setter
+public class Product {
+
+  @Id
+  private Long Id;
+  @Column(name = "product_name", length = 50, nullable = false)
+  private String productName;
+  @Column(name = "product_Id", length = 50, nullable = false)
+  private String product_Id;
+  @Column(name = "season", length = 50, nullable = false)
+  private String season;
+  @Column(name = "color", length = 50, nullable = false)
+  private String color;
+  @Column(name = "customer", length = 50, nullable = false)
+  private String customer;
+
+
+}
+
+
 
 
 
