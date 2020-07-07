e334e3dc93bb480de2c4ce91c240ef694dcf1ac8
emubaiwa <edwardmubaiwa@gmail.com>
2020-07-07 16:42:12 +0200

    added annotation to cater for constructor all all its arguments

--- src/main/java/com/cb/farmerapp/project/Project.java
+++ src/main/java/com/cb/farmerapp/project/Project.java
@@ -1,31 +1,29 @@
 package com.cb.farmerapp.project;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import com.cb.farmerapp.farmer.model.Farmer;
-import lombok.Getter;
-import lombok.Setter;
-import org.springframework.stereotype.Service;
-
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
 import java.time.LocalDate;
 
  @Entity
- @Service
  @Getter @Setter
-
+ @Table(name = "project")
+ @AllArgsConstructor
 public class Project {
   @Id
-  @Column(name = "projectId", length = 50, nullable = false,unique = true)
+  @Column(name = "projectId", length = 5, nullable = false,unique = true)
   private Long Id;
    @Column(name = "projectName", length = 50, nullable = false)
   private String projectName;
-   @Column(name = "projectDescription", length = 50, nullable = false)
+   @Column(name = "projectDescription", length = 200, nullable = false)
   private String description;
-   @Column(name = "projectBudget", length = 50, nullable = false)
+   @Column(name = "projectBudget", length = 5, nullable = false)
   private Double projectBudget;
-   @Column(name = "projectStartDate", length = 50, nullable = false)
+   @Temporal(value = TemporalType.TIMESTAMP)
+   @JsonFormat(pattern = "MM/dd/yyyy")
+   @Column(name = "projectStartDate", nullable = false)
   private LocalDate startDate;
-   @Column(name = "projectOwner", length = 50, nullable = false)
+   @Column(name = "projectOwner", length = 100, nullable = false)
   private String Owner;
    @Column(name = "projectManager", length = 50, nullable = false)
   private String projectManager;
@@ -35,24 +33,5 @@ public class Project {
   private String sponsor;
    @Column(name = "projectFarmer", length = 50, nullable = false)
   private Farmer farmer;
-
-  public Project(Long id, String projectName, String description, Double projectBudget, LocalDate startDate, String owner,
-                 String projectManager, String projectLocation, String sponsor) {
-    Id = id;
-    this.projectName = projectName;
-    this.description = description;
-    this.projectBudget = projectBudget;
-    this.startDate = startDate;
-    Owner = owner;
-    this.projectManager = projectManager;
-    this.projectLocation = projectLocation;
-    this.sponsor = sponsor;
-  }
-
-  public Project(String projectName, Double projectBudget) {
-    this.projectName = projectName;
-    this.projectBudget = projectBudget;
-
-  }
 
 }
