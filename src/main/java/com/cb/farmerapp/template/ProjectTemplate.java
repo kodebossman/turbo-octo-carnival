a226ed361987278a577109c6585989d3d8c0e277
tatenda <tmashinge97@gmail.com>
2020-07-07 14:53:57 +0200

    removed getters and setters added lombook and annotations

--- src/main/java/com/cb/farmerapp/template/ProjectTemplate.java
+++ src/main/java/com/cb/farmerapp/template/ProjectTemplate.java
@@ -1,15 +1,26 @@
 package com.cb.farmerapp.template;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
-
+import org.springframework.data.annotation.Id;
+import javax.persistence.*;
 import java.util.Date;
-
+@Entity
+@Table(name = "template")
 @Getter
 @Setter
+@Data
 public class ProjectTemplate {
+  @Id
+  private Long Id;
+  @Column (name = "Project_objective",length=50 ,nullable=false)
   private String ProjectObjective;
-  private Date ProjectDatestarted;
+  @Column (name = "Date_started", length =50, nullable =false)
+  private Date Dated_Started;
+  @Column (name ="Budgeted_resources",length = 50,nullable=false)
   private int ProjectBudgetedResources;
+  @Column (name ="Project_customer", length= 50,nullable= false)
   private String ProjectCustomer;
+
 }
